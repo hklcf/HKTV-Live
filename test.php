c3df97b4a25e4e1435f84ce117e987dafa5ae611
HKLCF <hklcfs@gmail.com>
2014-11-12 15:22:12 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,13 @@
 <?php
 $json = 'https://www.hktvmall.com/ott/token';
-$result = json_decode(file_get_contents($json, true));
+$result = json_decode(file_get_contents($json), true);
 
+/*
 foreach($result as $value) {
     echo $value['token'] . " ";
     echo $value['expiry_date'] . '<br>';
 }
-//var_dump($result);
+*/
+
+var_dump($result);
 ?>
