e850c23726b82423a562069ddea8bc6bb4085c21
HKLCF <hklcfs@gmail.com>
2014-11-12 17:20:51 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
-$json = 'https://www.hktvmall.com/ott/token';
-$result = json_decode(file_get_contents($json), true);
-$token = $result['token'];
-$expiry_date = $result['expiry_date']);
+$ott_token_json = 'https://www.hktvmall.com/ott/token';
+$ott_token_result = json_decode(file_get_contents($ott_token_json), true);
+$ott_uid = $ott_token_result['user_id'];
+$ott_token = $ott_token_result['token'];
+$ott_expiry_date = $ott_token_result['expiry_date']);
+
+$playlist_json = 'https://ott-www.hktvmall.com/api/playlist/request?uid={$ott_uid}&vid=1&t={$ott_token}&ppos=0&_={$ott_expiry_date}';
+$playlist_result = json_decode(file_get_contents($playlist_json), true);
 ?>
