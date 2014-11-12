a026213c7e69a5dac40b4c3c930dd4fbc725c6e5
HKLCF <hklcfs@gmail.com>
2014-11-12 17:34:03 +0800

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $ott_token_result = json_decode(file_get_contents($ott_token_json), true);
 $ott_uid = $ott_token_result['user_id'];
 $ott_token = $ott_token_result['token'];
 $ott_expiry_date = $ott_token_result['expiry_date'];
-
-$playlist_json = 'https://ott-www.hktvmall.com/api/playlist/request?uid={$ott_uid}&vid=1&t={$ott_token}&ppos=0&_={$ott_expiry_date}';
+$playlist_json = "https://ott-www.hktvmall.com/api/playlist/request?uid={$ott_uid}&vid=1&t={$ott_token}&ppos=0&_={$ott_expiry_date}";
 $playlist_result = json_decode(file_get_contents($playlist_json), true);
+header("Location: $playlist_result['m3u8']");
 ?>
