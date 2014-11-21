89d35f2b472b0ceb1fbc695b949ea06bd74f4824
HKLCF <hklcfs@gmail.com>
2014-11-21 20:32:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 $ott_token_json = 'https://www.hktvmall.com/ott/token';
 $ott_token_result = json_decode(file_get_contents($ott_token_json), true);
 $ott_uid = $ott_token_result['user_id'];
+$ott_vid = $_GET['vid'] ? $_GET['vid'] : '1';
 $ott_token = $ott_token_result['token'];
 $ott_expiry_date = $ott_token_result['expiry_date'];
-$playlist_json = "https://ott-www.hktvmall.com/api/playlist/request?uid={$ott_uid}&vid=1&t={$ott_token}&ppos=0&_={$ott_expiry_date}";
+$playlist_json = "https://ott-www.hktvmall.com/api/playlist/request?uid={$ott_uid}&vid={$ott_vid}&t={$ott_token}&ppos=0&_={$ott_expiry_date}";
 $playlist_result = json_decode(file_get_contents($playlist_json), true);
 if(isset($_GET['q'])) {
     if($_GET['q'] == '1080') {
