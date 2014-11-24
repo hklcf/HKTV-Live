0375315cb76143cd9217761014369d3d00791592
HKLCF <hklcfs@gmail.com>
2014-11-24 18:54:48 +0800

    Update csrf_token.php

--- csrf_token.php
+++ csrf_token.php
@@ -1,8 +1,8 @@
 <?php
-$debug = file_get_contents('https://www.hktvmall.com/hktv/zh/login', true);
-$debug_s = strpos($debug, 'CSRFToken');
-$debug_e = strpos($debug, '"', $debug_s + 18);
-$csrf_token = substr($debug,$debug_s+18,$debug_e-strlen($debug));
+$csrf_token_url = file_get_contents('https://www.hktvmall.com/hktv/zh/login', true);
+$csrf_token_s = strpos($csrf_token_url, 'CSRFToken');
+$csrf_token_e = strpos($csrf_token_url, '"', $csrf_token_s + 18);
+$csrf_token = substr($csrf_token_url, $csrf_token_s + 18, $csrf_token_e - strlen($csrf_token_url));
 
 print($csrf_token);
 ?>
