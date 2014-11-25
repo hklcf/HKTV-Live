e235dc8a40826a5134eab1ce2b759be041d569ca
HKLCF <hklcfs@gmail.com>
2014-11-25 23:52:25 +0800

    Update old.php

--- old.php
+++ old.php
@@ -1,24 +1,24 @@
 <?php
 function generateSignature($file, $time, $qsv) {
-	return md5($file.$qsv.'43e814b31f8764756672c1cd1217d775'.$time);
+    return md5($file.$qsv.'43e814b31f8764756672c1cd1217d775'.$time);
 }
 $url = 'http://webservices.hktv.com.hk/account/token';
 $ki = '12';
 $ts = date(U);
 $s = generateSignature('account/token',$ts,$ki.'0');
 $data = array(
-	"muid" => "0",
-	"ki" => $ki,
-	"ts" => $ts,
-	"s"=> $s
+    "muid" => "0",
+    "ki" => $ki,
+    "ts" => $ts,
+    "s"=> $s
 );
 // use key 'http' even if you send the request to https://...
 $options = array(
-	'http' => array(
-		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		'method'  => 'POST',
-		'content' => http_build_query($data),
-	),
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
 );
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
@@ -38,26 +38,26 @@ $s2 = generateSignature('playlist/request',$ts,$d.$ki.$mdl.$mf.$mxres.$net.$os.$
 //$url2 = 'http://webservices.hktv.com.hk/playlist/request';
 $url2 = 'http://ott-www.hktvmall.com/api/playlist/request';
 $data2 = array(
-	"d" => $d,
-	"ki" => $ki,
-	"mdl" => $mdl,
-	"mf" => $mf,
-	"mxres" => $mxres,
-	"net" => $net,
-	"os" => $os,
-	"t" => $got_token,
-	"udid" => $udid,
-	"uid" => $uid,
-	"vid" => $vid,
-	"ts" => $ts,
-	"s"=> $s2
+    "d" => $d,
+    "ki" => $ki,
+    "mdl" => $mdl,
+    "mf" => $mf,
+    "mxres" => $mxres,
+    "net" => $net,
+    "os" => $os,
+    "t" => $got_token,
+    "udid" => $udid,
+    "uid" => $uid,
+    "vid" => $vid,
+    "ts" => $ts,
+    "s"=> $s2
 );
 $options2 = array(
-	'http' => array(
-		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		'method'  => 'POST',
-		'content' => http_build_query($data2),
-	),
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data2),
+    ),
 );
 $context2  = stream_context_create($options2);
 $result2 = file_get_contents($url2, false, $context2);
