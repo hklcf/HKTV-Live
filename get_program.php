5cd02be1d8ba99b9904476166433ccf008b7c685
HKLCF <hklcfs@gmail.com>
2014-11-26 19:50:03 +0800

    Update get_program.php

--- get_program.php
+++ get_program.php
@@ -8,7 +8,7 @@ foreach($lists_json['videos'] as $program => $program_value) {
         print_r($key_value['title']."-");
         //print_r($key_value);
         foreach($key_value['child_nodes'] as $y => $y_value) {
-            print_r($y_value['title']."http://live.eservice-hk.net/hktv?vid=".$y_value['video_id']."\r\n");
+            print_r($y_value['title']."http://live.eservice-hk.net/hktv?vid=".$y_value['video_id']."|");
             echo "\r\n";
         }
     }
