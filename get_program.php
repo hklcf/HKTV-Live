6af9ea47667f292776a115fb55c329f2a0ec58af
HKLCF <hklcfs@gmail.com>
2014-11-26 23:58:18 +0800

    Update get_program.php

--- get_program.php
+++ get_program.php
@@ -1,7 +1,7 @@
 <?php
 $url = 'http://live.eservice-hk.net/hktv?vid=';
-$lists_page = $_GET['page'] ? $_GET['page'] : '0';
-$lists_ofs = $lists_page * '6';
+$lists_page = $_GET['page'] ? $_GET['page'] : '1';
+$lists_ofs = $lists_page * '6' - '6';
 $lists_json = json_decode(file_get_contents("https://ott-www.hktvmall.com/api/lists/getProgram?lim=6&ofs={$lists_ofs}"), true);
 foreach($lists_json['videos'] as $program_x => $program_x_value) {
     foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
