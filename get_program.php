035bf6a66c52b58d6890955d59681ff59209676a
HKLCF <hklcfs@gmail.com>
2014-12-01 02:13:05 +0800

    Update get_program.php

--- get_program.php
+++ get_program.php
@@ -13,22 +13,27 @@ $lists_page = $_GET['page'] ? $_GET['page'] : '1';
 $lists_lim = $_GET['lim'] ? $_GET['lim'] : '6';
 $lists_ofs = $lists_page * $lists_lim - $lists_lim;
 $lists_json = json_decode(file_get_contents("https://ott-www.hktvmall.com/api/lists/getProgram?lim={$lists_lim}&ofs={$lists_ofs}"), true);
-foreach($lists_json['videos'] as $program_x => $program_x_value) {
-    echo "<h1>{$program_x_value['title']}</h1>";
-    echo "<br>";
-    echo "<img src='{$program_x_value['thumbnail']}' alt='{$program_x_value['title']}'>";
-    echo "<br>";
-    foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
-        echo "<span>{$program_y_value['title']}</span>";
+$lists_total_page = ceil($lists_json['total_videos']/$lists_lim);
+if($lists_page <= $lists_total_page) {
+    foreach($lists_json['videos'] as $program_x => $program_x_value) {
+        echo "<h1>{$program_x_value['title']}</h1>";
         echo "<br>";
-        foreach($program_y_value['child_nodes'] as $program_z => $program_z_value) {
-            echo "<a href='{$url}{$program_z_value['video_id']}'>{$program_z_value['title']}</a>";
+        echo "<img src='{$program_x_value['thumbnail']}' alt='{$program_x_value['title']}'>";
+        echo "<br>";
+        foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
+            echo "<span>{$program_y_value['title']}</span>";
             echo "<br>";
+            foreach($program_y_value['child_nodes'] as $program_z => $program_z_value) {
+                echo "<a href='{$url}{$program_z_value['video_id']}'>{$program_z_value['title']}</a>";
+                echo "<br>";
+            }
         }
+        echo "<br>";
     }
-    echo "<br>";
+    echo "Page {$lists_page} of {$lists_total_page}"; // page
+} else {
+    echo "Page not found";
 }
-//echo ceil($lists_json['total_videos']/$lists_lim); // page
 ?>
 </body>
 
