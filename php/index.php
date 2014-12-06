7e67e899029f54aac451958e3a0fef8611e4b1c9
HKLCF <hklcf@users.noreply.github.com>
2014-12-06 23:36:44 +0800

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-$account = mt_rand(0, 4);
+$account = mt_rand(0, 5);
 $username = array('osm01288@soisz.com', 'lyu33964@soisz.com', 'rvp89615@soisz.com', 'swk41652@soisz.com', 'whc27702@soisz.com', 'b3552886@trbvm.com');
 $password = array('q1w2e3r4', 'q1w2e3r4', 'q1w2e3r4', 'q1w2e3r4', 'q1w2e3r4', 'q1w2e3r4');
 $login_ch = curl_init();
