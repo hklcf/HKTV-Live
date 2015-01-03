aea1b9a9589614230e9c6503465b629971daf4c9
HKLCF <hklcf@users.noreply.github.com>
2015-01-04 03:47:11 +0800

    Update download.php

--- php/download.php
+++ php/download.php
@@ -2,8 +2,8 @@
 $url = 'http://live.eservice-hk.net/hktv?vid=';
 if(isset($_GET['vid']) && isset($_GET['name'])) {
     $content = "@echo off\r\n";
-    $content .= "title 正在下載 {$program_z_value['title']}\r\n";
-    $content .= 'ffmpeg -i '."{$url}{$program_z_value['video_id']}".' -c copy '."{$program_z_value['title']}".'.ts"';
+    $content .= "title 正在下載 {$_GET['name']}\r\n";
+    $content .= 'ffmpeg -i "'."{$url}{$_GET['vid']}".'" -c copy "'."{$_GET['name']}".'.ts"';
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream; charset=UTF-8');
     header('Content-Disposition: attachment; filename='.$program_z_value['title'].'.bat');
