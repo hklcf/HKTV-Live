2b7bc91d79996c24c1f22f72b91f468db8201f8d
HKLCF <hklcf@users.noreply.github.com>
2015-01-04 04:18:13 +0800

    Update download.php

--- php/download.php
+++ php/download.php
@@ -1,12 +1,13 @@
 <?php
 $url = 'http://live.eservice-hk.net/hktv?vid=';
-if(isset($_GET['vid']) && isset($_GET['name'])) {
+$name = urlencode($_GET['name']));
+if(isset($_GET['vid']) && isset($name))) {
     $content = "@echo off\r\n";
-    $content .= "title 正在下載 {$_GET['name']}\r\n";
-    $content .= 'ffmpeg -i "'."{$url}{$_GET['vid']}".'" -c copy "'."{$_GET['name']}".'.ts"';
+    $content .= "title 正在下載 $name\r\n";
+    $content .= 'ffmpeg -i "'."{$url}{$_GET['vid']}".'" -c copy "'."$name".'.ts"';
     header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream; charset=UTF-8');
-    header('Content-Disposition: attachment; filename='.$_GET['name'].'.bat');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.$name.'.bat');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
