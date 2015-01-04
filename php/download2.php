e04c166c29a375f6e926b04d372800def380c3f0
HKLCF <hklcf@users.noreply.github.com>
2015-01-04 14:00:56 +0800

    Update download2.php

--- php/download2.php
+++ php/download2.php
@@ -21,41 +21,32 @@ if(isset($_GET['vid']) && isset($_GET['name'])) {
 
 <?php
 $url = 'http://live.eservice-hk.net/hktv?vid=';
-$lists_page = $_GET['page'] ? $_GET['page'] : '1';
 $lists_lim = $_GET['lim'] ? $_GET['lim'] : '6';
 $lists_ofs = $lists_page * $lists_lim - $lists_lim;
 $lists_json = json_decode(file_get_contents("https://ott-www.hktvmall.com/api/lists/getProgram?lim={$lists_lim}&ofs={$lists_ofs}"), true);
 $lists_total_page = ceil($lists_json['total_videos']/$lists_lim);
-if($lists_page <= $lists_total_page) {
-    foreach($lists_json['videos'] as $program_x => $program_x_value) {
-        rsort($program_x_value['child_nodes']);
-        echo "<h1>{$program_x_value['title']}</h1>";
+foreach($lists_json['videos'] as $program_x => $program_x_value) {
+    rsort($program_x_value['child_nodes']);
+    foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
+        sort($program_y_value['child_nodes']);
+        echo "<span>{$program_y_value['title']}</span>";
         echo "<br>";
-        echo "<img src='{$program_x_value['thumbnail']}' alt='{$program_x_value['title']}'>";
-        echo "<br>";
-        foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
-            sort($program_y_value['child_nodes']);
-            echo "<span>{$program_y_value['title']}</span>";
+        foreach($program_y_value['child_nodes'] as $program_z => $program_z_value) {
+            echo "<a href='{$url}{$program_z_value['video_id']}'>{$program_z_value['title']}</a>";
+            echo " | ";
+            echo "<a href='download.php?vid={$program_z_value['video_id']}&name=".urlencode($program_z_value['title'])."'>Download</a>";
             echo "<br>";
-            foreach($program_y_value['child_nodes'] as $program_z => $program_z_value) {
-                echo "<a href='{$url}{$program_z_value['video_id']}'>{$program_z_value['title']}</a>";
-                echo " | ";
-                echo "<a href='download.php?vid={$program_z_value['video_id']}&name=".urlencode($program_z_value['title'])."'>Download</a>";
-                echo "<br>";
-            }
         }
-        echo "<br>";
     }
-    if($lists_page > '1') {
-        $lists_pervious_page = $lists_page - '1';
-        echo "<a href='?page={$lists_pervious_page}'><&nbsp;</a>";
-    }
-    echo "Page {$lists_page} of {$lists_total_page}";
-    if($lists_page < $lists_total_page) {
-        $lists_next_page = $lists_page + '1';
-        echo "<a href='?page={$lists_next_page}'>&nbsp;></a>";
-    }
-} else {
-    echo "Page not found";
+    echo "<br>";
+}
+if($lists_page > '1') {
+    $lists_pervious_page = $lists_page - '1';
+    echo "<a href='?page={$lists_pervious_page}'><&nbsp;</a>";
+}
+echo "Page {$lists_page} of {$lists_total_page}";
+if($lists_page < $lists_total_page) {
+    $lists_next_page = $lists_page + '1';
+    echo "<a href='?page={$lists_next_page}'>&nbsp;></a>";
 }
 ?>
