83df428ead667d5f6bafb344a61eea9aa34ffb56
HKLCF <hklcf@users.noreply.github.com>
2015-01-05 23:46:15 +0800

    Update download2.php

--- php/download2.php
+++ php/download2.php
@@ -1,19 +1,22 @@
 <?php
-$name = urldecode($_GET['name']);
-if(isset($_GET['content']) && isset($_GET['name'])) {
-    $content = "@echo off\r\n";
-    $content .= "chcp 65001\r\n";
-    $content .= "title 正在下載 $name\r\n";
-    $content .= $_GET['content'];
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.$name.'.bat');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($content));
-    echo $content;
-} else {
-    echo "Download Error!";
+$url = 'http://live.eservice-hk.net/hktv?vid=';
+$lists_page = $_GET['page'] ? $_GET['page'] : '1';
+$lists_lim = $_GET['lim'] ? $_GET['lim'] : '6';
+$lists_ofs = $lists_page * $lists_lim - $lists_lim;
+$lists_json = json_decode(file_get_contents("https://ott-www.hktvmall.com/api/lists/getProgram?lim={$lists_lim}&ofs={$lists_ofs}"), true);
+foreach($lists_json['videos'] as $program_x => $program_x_value) {
+    rsort($program_x_value['child_nodes']);
+    foreach($program_x_value['child_nodes'] as $program_y => $program_y_value) {
+        sort($program_y_value['child_nodes']);
+        if($program_y_value['video_id'] == $_GET['vid']) {
+            foreach($program_y_value['child_nodes'] as $program_z => $program_z_value) {
+                echo "<a href='{$url}{$program_z_value['video_id']}'>{$program_z_value['title']}</a>";
+                echo " | ";
+                echo "<a href='download.php?vid={$program_z_value['video_id']}&name=".urlencode($program_z_value['title'])."'>Download</a>";
+                echo "<br>";
+            }
+        }
+    }
+    echo "<br>";
 }
 ?>
